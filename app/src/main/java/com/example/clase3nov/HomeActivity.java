2746ea0f0b56bbb197047b0ccb47a7a4239a07a6
dalgyd <79942372+dalgyd@users.noreply.github.com>
2021-11-04 07:44:28 -0500

    Initial commit

---
+++ app/src/main/java/com/example/clase3nov/HomeActivity.java
@@ -0,0 +1,20 @@
+package com.example.clase3nov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        getSupportActionBar().hide();//Oculta barra superior
+
+        
+    }
+}
