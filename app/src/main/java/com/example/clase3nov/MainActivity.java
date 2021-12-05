119dd3d44002ddc5bf97c00f2cd931d9ceef14ed
dalgyd <79942372+dalgyd@users.noreply.github.com>
2021-12-05 00:41:48 -0500

    Cambios

--- app/src/main/java/com/example/clase3nov/MainActivity.java
+++ app/src/main/java/com/example/clase3nov/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,19 +24,68 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     //la declaramos de forma global
-    Button loginbtn;
+    //Button loginbtn; //declaracion del boton para login
+
+    // Declaraciones para un boton que muestre un cuadro de dialogo
+    private Dialog dialog;
+    private Button ShowDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ShowDialog = findViewById(R.id.loginbtn); // boton de cuadro de dialogo
+
         getSupportActionBar().hide();
 
         TextView username = (TextView) findViewById(R.id.username);
         TextView password = (TextView) findViewById(R.id.password);
 
-        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn); //este es para el boton de login
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn); //este es para el boton de login y cuadro de dialogo
+        Dialog dialog; //Nuevo para el dialogo
+
+        //conectamos la clase .java para el cuadro de dialogo
+
+        dialog = new Dialog(MainActivity.this); //Indicamos donde se mostrara el dialogo
+        // conectamos la .xml de dialogo
+        dialog.setContentView(R.layout.custom_dialog); //Coneion al archivo custom:dialog.xml
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            dialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.background2));
+        }
+
+        dialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.background2));//Asignamos el fondo del dialogo
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.setCancelable(false);
+
+        //Declaramos los botones
+        Button Okey = dialog.findViewById(R.id.btn_okay);
+        Button Cancel = dialog.findViewById(R.id.btn_cancel);
+
+        // Se declaran las acciones de los botones
+        Okey.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Okay", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+
+        Cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Cancel", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+
+        //Se muestra el dialogo
+        ShowDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.show();
+            }
+        });
 
         // loginbtn = findViewById(R.id.loginbtn); para el boton con la base de datos
 
@@ -54,6 +106,7 @@ public class MainActivity extends AppCompatActivity {
     }*/
 
 
+        /* Codigo del boton login
         //Codigo del boton tipo login
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,7 +117,9 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "Usuario no registrado", Toast.LENGTH_SHORT).show();
                 }
             }
-        });
+        });*/
+
+
     }
 
     public void regis (View m){
